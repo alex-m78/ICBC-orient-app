9663d6dd122785bfc0ae1b66ceffe8f8ea194717
hkd <3085870045@qq.com>
2020-08-10 16:47:10 +0800

    update test codes

--- orient-app/src/test/java/com/icbc/orient/Service/JwtUserServiceTest.java
+++ orient-app/src/test/java/com/icbc/orient/Service/JwtUserServiceTest.java
@@ -1,32 +1,46 @@
 package com.icbc.orient.Service;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
-
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class JwtUserServiceTest {
+
+    @Autowired
+    JwtUserService jwtUserService;
 
     @Test
     public void loadUserByUsername() {
+        jwtUserService.loadUserByUsername("Zhangyide");
     }
 
     @Test
     public void loadAllUser() {
+        jwtUserService.loadAllUser();
     }
 
     @Test
     public void deleteUser() {
+        jwtUserService.DeleteUser("user");
     }
 
     @Test
     public void editUserRole() {
+        jwtUserService.editUserRole("user","admin");
     }
 
     @Test
     public void addUser() {
+        jwtUserService.addUser("user","123456","15888888888");
     }
 
     @Test
     public void checkRoleByUsername() {
+        jwtUserService.CheckRoleByUsername("user");
     }
 }
--- orient-app/src/test/java/com/icbc/orient/Service/TargetServiceTest.java
+++ orient-app/src/test/java/com/icbc/orient/Service/TargetServiceTest.java
@@ -1,28 +1,52 @@
 package com.icbc.orient.Service;
 
+
+import com.icbc.orient.Bean.Target;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
-
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class TargetServiceTest {
+
+    @Autowired
+    TargetService targetService;
 
     @Test
     public void totalTarget() {
+        targetService.TotalTarget();
+
     }
 
     @Test
     public void profitTarget() {
+
+        targetService.ProfitTarget();
+
     }
 
     @Test
     public void developTarget() {
+
+        targetService.DevelopTarget();
+
     }
 
     @Test
     public void valueTarget() {
+
+        targetService.ValueTarget();
+
     }
 
     @Test
     public void storeFeedback() {
+
+        targetService.StoreFeedback("Test if This feature is ok");
+
     }
 }
